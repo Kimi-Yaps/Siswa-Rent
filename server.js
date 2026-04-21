b79a0fa8cc25bab88bd370a48150c41eb8904ab1
Kimi-Yaps <168716465+Kimi-Yaps@users.noreply.github.com>
2026-04-21 17:04:53 +0800

    update server

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ import path from 'path';
 const app = express();
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
+
 
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0', () => console.log(`Listening on port ${port}`));
